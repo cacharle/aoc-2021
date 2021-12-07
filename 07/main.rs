f5f49583dacc03183028d554ffdb7ebecdf367f5
Charles Cabergs <me@cacharle.xyz>
2021-12-07 07:22:49 +0100

    Added Day 07

---
+++ 07/main.rs
@@ -0,0 +1,62 @@
+use std::error::Error;
+use std::fs;
+use std::str::FromStr;
+
+const INPUT_FILENAME: &str = "07/input.txt";
+
+#[derive(Clone)]
+struct Crabs(Vec<i32>);
+
+use std::cmp::min;
+impl Crabs {
+    fn fuel_cost_part1(&self, pos: i32) -> i32 {
+        self.0.iter().map(|p| (p - pos).abs()).sum()
+    }
+
+    /*
+     * PERF: can probably replace the summation in map by a smarter formula
+     *       right now, we need -O3, it taks 45s with -O0
+     * f(x) = f(x - 1) + 1
+     * f(1) = 1
+     */
+    fn fuel_cost_part2(&self, pos: i32) -> i32 {
+        self.0
+            .iter()
+            .map(|p| (1..(p - pos).abs() + 1).sum::<i32>())
+            .sum()
+    }
+}
+
+impl FromStr for Crabs {
+    type Err = Box<dyn Error>;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let nums = s
+            .trim()
+            .split(",")
+            .map(|n| n.parse())
+            .collect::<Result<_, _>>()?;
+        Ok(Crabs(nums))
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let file_content = fs::read_to_string(INPUT_FILENAME)?;
+    let crabs: Crabs = file_content.parse()?;
+
+    let min = *crabs.0.iter().min().ok_or("no crabs pos")?;
+    let max = *crabs.0.iter().max().ok_or("no crabs pos")?;
+
+    let min_fuel_cost = (min..max)
+        .map(|pos| crabs.fuel_cost_part1(pos))
+        .min()
+        .ok_or("couldn't find min fuel cost")?;
+    println!("part1 result: {}", min_fuel_cost);
+
+    let min_fuel_cost = (min..max)
+        .map(|pos| crabs.fuel_cost_part2(pos))
+        .min()
+        .ok_or("couldn't find min fuel cost")?;
+    println!("part2 result: {}", min_fuel_cost);
+
+    Ok(())
+}
